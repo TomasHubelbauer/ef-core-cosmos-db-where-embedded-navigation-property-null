57e1db067134174ce8359a6e07d4fe6b06b86fd7
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-05-20 13:00:17 +0200

    Attempt to reproduce the problem

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ef_core_cosmos_db_where_embedded_navigation_property_null
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      // Seed the database
+      using (var appDbContext = new AppDbContext())
+      {
+        await appDbContext.Database.EnsureDeletedAsync();
+        await appDbContext.Database.EnsureCreatedAsync();
+        await appDbContext.Items.AddAsync(new Item
+        {
+          Name = "sole item",
+          Metadata = new Metadata
+          {
+            CreatedBy = "me",
+            UpdatedBy = "me"
+          }
+        });
+
+        await appDbContext.SaveChangesAsync();
+      }
+
+      // Reproduce the problem knowing we have a single `Item` entity with a non-`null` `Metadata` navigation property
+      using (var appDbContext = new AppDbContext())
+      {
+        var allItems = await appDbContext.Items.ToArrayAsync();
+        var items = appDbContext.Items.Where(item => item.Metadata != null).ToArray();
+        Console.WriteLine($"{nameof(allItems)} count: {allItems.Length}");
+
+        // TODO: Distill more of the real application so that this reproduces by returning a zero
+        Console.WriteLine($"{nameof(items)} count: {items.Length}");
+      }
+    }
+  }
+
+  class AppDbContext : DbContext
+  {
+    public DbSet<Item> Items { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      Console.WriteLine("Enter the CosmosDB Emulator primary key:");
+      optionsBuilder.UseCosmos("https://localhost:8081", Console.ReadLine(), nameof(ef_core_cosmos_db_where_embedded_navigation_property_null));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Item>(entity => entity.OwnsOne(item => item.Metadata));
+    }
+  }
+
+  class Item
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Metadata Metadata { get; set; }
+  }
+
+  class Metadata
+  {
+    public Guid Id { get; set; }
+    public string CreatedBy { get; set; }
+    public string UpdatedBy { get; set; }
+  }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# EF Core CosmosDB `Where` Embedded Navigation Property `null`
+
+In this repository I aim to analyze an apparent issue where CosmosDB EF Core provider's `Where` clause implementation
+provides an argument to the predicate whose embedded navigation property resolves to `null` but after materialization of
+the query, the entities have their embedded navigation properties materialized as well.
+
+An example code where this could be triggered follows:
+
+```csharp
+var items = appDbContext.Items.Where(i => i.Metadata != null).ToArray();
+```
+
+In the above code, the query will return zero items even though an item exists whose `Metadata` property resolves to an
+embedded entity which is not `null`. Executing the query without the `Where` clause, the materialized entity will not
+have a `null` for the navigation property value.
+
+This is hard to debug, because the EF Core provider expects an `Expression` not an actual `Func` (because it is analyzed
+and turned into a SQL query string, not ever actually executed) so it is impossible to make the predicate multi-line and
+place a breakpoint within to analyze the arguments one-by-one.
+
+The reproduction in this repository is a simple .NET Core console application bootstrapped using `dotnet new console`.
+The CosmosDB EF Core provider used is the `Microsoft.EntityFrameworkCore.Cosmos` NuGet package.
+
+I've tried this with both version 2 and version 3 (`3.0.0-preview4.19216.3`) and I am unable to reproduce, so I must be
+missing something which causes this to happen in the real application but is missing in my repro.
